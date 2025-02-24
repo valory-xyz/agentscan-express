540e152da0f5eb058fd178899050127ca55f37d4
kevin <kevinoconnell42@gmail.com>
2025-02-24 14:13:41 +0000

    delete server

--- src/services/misc.ts
+++
@@ -1,7 +0,0 @@
-export function encodeCursor(cursor: string): string {
-  return Buffer.from(cursor).toString("base64");
-}
-
-export function decodeCursor(encodedCursor: string): string {
-  return Buffer.from(encodedCursor, "base64").toString("ascii");
-}
