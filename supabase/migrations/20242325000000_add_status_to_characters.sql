8f0781ed927bada6647e3592ab4dec9f0c3359ea
kevin <kevinoconnell42@gmail.com>
2025-02-24 14:32:09 +0000

    delete supabase

--- supabase/migrations/20242325000000_add_status_to_characters.sql
+++
@@ -1,8 +0,0 @@
--- Add status column to characters table
-
-
--- Add not null constraint
-ALTER TABLE public.characters ALTER COLUMN status SET NOT NULL;
-
--- Revert changes if needed
--- ALTER TABLE public.characters DROP COLUMN status;
--- supabase/seed.sql
+++
@@ -1,2 +0,0 @@
-
-insert into seasons (name, is_current, is_playing, biome, season_number) values ('Season 1', true, false, 'tundra', 1);
