ba0ccf0db7dd566c7c76f3f33e6a7c23c2852132
Oscar Perez <ooscar.perez@protonmail.com>
2025-03-19 15:36:35 +0100

    project: disable amplitude analytics service

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import express from "express";
 import { createServer } from "http";
 import { config } from "./config";
-import { redis } from "./initalizers/redis";
-
 import cors from "cors";
-import { pool } from "./initalizers/postgres";
-import privy from "./initalizers/privy";
-
-import { getUsersByIds } from "./services/user";
-
 import dotenv from "dotenv";
-import { Socket } from "socket.io";
-import { amplitudeClient } from "./initalizers/amplitude";
 import { initializeDiscord } from "./initalizers/discord";
 import { initializeTelegram } from "./initalizers/telegram";
 
@@ -28,7 +19,6 @@ app.use(cors());
 app.use("/", require("./routes").default);
 
 async function initServer(): Promise<void> {
-  amplitudeClient;
   httpServer.listen(config.server.port, () => {
     console.log(`Server running on port ${config.server.port}`);
   });
