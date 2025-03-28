3299e00180082902ea929d967039d194bd52c8cc
Oscar Perez <ooscar.perez@protonmail.com>
2025-03-28 12:34:27 +0100

    fix cors missing

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Configure to allow only from agentscan.com
-app.use();
+app.use(cors());
 
 app.use("/", require("./routes").default);
 
